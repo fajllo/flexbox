55693f56846933a22978836dbe7d24cb26c1b958
fsdr <filipsordyl8@unilever.com>
2020-11-18 20:48:50 +0100

    little change

--- nav.js
+++ nav.js
@@ -1,9 +1,13 @@
 let a = document.querySelectorAll('a');
+let main = document.querySelector('main');
+let h2 = document.querySelector('h2');
+console.dir(a);
 for (let i of a) {
   i.addEventListener('click', (e) => {
     for (let j of a) {
       j.classList.remove('active');
     }
     i.classList.add('active');
+    h2.innerHTML = i.innerHTML;
   });
 }
