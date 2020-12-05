0c3dfb41957fa1840f33004403fae9a8a09196fb
fsdr <filipsordyl8@unilever.com>
2020-12-05 22:55:01 +0100

    home page rady

--- hamburger/app.js
+++ hamburger/app.js
@@ -3,12 +3,15 @@ let navbar = document.querySelector('.ul-nav');
 let plane = document.querySelector('.plane-text');
 let encrypted = document.querySelector('.encrypted');
 let copy = document.querySelector('.copy');
+let picker = document.querySelectorAll('.pick-cypher');
+let h1 = document.querySelectorAll('h1');
+let a = document.querySelectorAll('.a-nav');
+
 hamburger.addEventListener('click', () => {
   navbar.classList.toggle('open');
 });
 
 plane.addEventListener('input', (e) => {
-  console.log(plane);
   encrypted.value = btoa(e.target.value);
   copy.innerText = 'COPY!';
 });
@@ -22,3 +25,22 @@ copy.addEventListener('click', (event) => {
   copyMe();
   event.target.innerText = 'Copied!';
 });
+
+for (let pick of picker) {
+  pick.addEventListener('click', (e) => {
+    for (let i of picker) {
+      i.classList.add('not-active');
+      i.classList.remove('active');
+    }
+    pick.classList.remove('not-active');
+    pick.classList.add('active');
+  });
+}
+for (let i of a) {
+  i.addEventListener('click', (e) => {
+    for (let j of a) {
+      j.classList.remove('current');
+    }
+    i.classList.add('current');
+  });
+}
